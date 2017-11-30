d8915395d2423bd34097dacc6dccb75306b60422
Gökhan Sarı <gokhan@gokhansari.me>
2017-11-30 11:51:17 +0300

    Add common response functions to api.

---
+++ api/common.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
+	response, err := json.Marshal(payload)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write([]byte(response))
+}
+
+func respondError(w http.ResponseWriter, status int, message string) {
+	respondJSON(w, status, map[string]string{"error": message})
+}
