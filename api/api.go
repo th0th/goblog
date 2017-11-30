298c24c76beca066d4b60df1d72fb8aec44a6cef
Gökhan Sarı <gokhan@gokhansari.me>
2017-11-30 11:46:38 +0300

    Add a simple working API example.

--- api/api.go
+++ api/api.go
@@ -1,1 +1,26 @@
 package api
+
+import (
+	"net/http"
+	"log"
+	"github.com/julienschmidt/httprouter"
+	"goblog/store"
+)
+
+type API struct {
+	router *httprouter.Router
+	store store.Store
+}
+
+func (a *API) init() {
+	a.router = httprouter.New()
+
+	a.router.GET("/categories/", ListCategories)
+}
+
+func (a *API) Run() {
+	a.init()
+
+	log.Fatal(http.ListenAndServe(":8080", a.router))
+}
+
---
+++ api/handlers.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"goblog/store"
+	"fmt"
+	"net/http"
+)
+
+func ListCategories(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	s := store.New()
+
+	data, err := s.CategoryService.List()
+
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+
+	fmt.Fprint(w, data)
+}
